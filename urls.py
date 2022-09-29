ea099f04bb1a546e4034e55d7efb07adb1ecbe81
ChinnuMoh23 <chinumohanan@gmail.com>
2022-09-29 14:20:59 +0530

    modified

--- urls.py
+++ urls.py
@@ -1,1 +1,9 @@
 mapping here
+git init---> for initializing git
+git status---> for list all untracked files
+ git add -A ---> add untracked file to sa
+git commit -m "msg" ---> add changes to localrepo
+
+git remote add name path ---> to add remoterepo
+git push -u name branch
+
