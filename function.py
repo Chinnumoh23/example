31ac2c82189dc6565e245237f0729f74632351ed
ChinnuMoh23 <chinumohanan@gmail.com>
2022-10-10 15:16:27 +0530

    newone

---
+++ function.py
@@ -0,0 +1,10 @@
+# class based
+class Loginview(View):
+    def get (self,request):
+        pass
+# urls= path("login,views.Loginview.as_view())
+
+def login(request):
+    pass
+# urls=path("login",views.login)
+
